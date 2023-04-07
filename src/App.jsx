064597e8b845a991364f1f7519667f5bb7925234
tharsila <tharsilapaula@gmail.com>
2023-04-07 17:24:50 -0300

    feat: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,1 +1,10 @@
-export { };
+import { Navigate, Route, Routes } from "react-router-dom"
+
+export const AppRoutes  = () => {
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<h1>Pagina Inicial</h1>} />
+            <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
+        </Routes>
+    )
+}
