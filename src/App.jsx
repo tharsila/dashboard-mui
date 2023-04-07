3c4f952ee1fb3779d87c13ccb1a4b71665260d44
tharsila <tharsilapaula@gmail.com>
2023-04-07 19:20:54 -0300

    feat: light theme e theme provider do mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
+import { ThemeProvider } from '@mui/material'
+import { LightTheme } from './shared/themes'
 
 
 function App() {
   
 
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 export const AppRoutes  = () => {
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<h1>Pagina Inicial</h1>} />
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Testando</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
         </Routes>
     )
