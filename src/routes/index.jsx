41b18973208eebb5b82dd3545d640a6c69e5446e
tharsila <tharsilapaula@gmail.com>
2023-05-25 20:56:21 -0300

    refactor: remove comentarios

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,14 @@
 import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { /* ThemeContext, */ useAppThemeContext } from '../shared/contexts/ThemeContext';
-/* import { useContext } from 'react'; */
+import { useAppDrawerContext } from '../shared/contexts/';
 
 export const AppRoutes  = () => {
-    // primeira forma de fazer >>>> const { toggleTheme } = useContext(ThemeContext)
-    const{ toggleTheme } = useAppThemeContext();
+
+    const { toggleDrawerOpen} = useAppDrawerContext();
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Testando</Button>} />
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Testando</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
         </Routes>
     );
